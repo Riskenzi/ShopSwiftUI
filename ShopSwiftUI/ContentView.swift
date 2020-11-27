f6e57de9265e91886d71b18f75cc437bee039aef
Valerii Melnykov <melnykov.valerii@gmail.com>
2020-11-27 11:05:29 +0200

    Add files via upload

---
+++ ShopSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ShopSwiftUI
+//
+//  Created by Valerii Melnykov on 27/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopSwiftUI/ShowSwiftUI.swift
@@ -0,0 +1,17 @@
+//
+//  SpecsApp.swift
+//  ShopSwiftUI
+//
+//  Created by Valerii Melnykov on 27/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ShowSwiftUI: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
